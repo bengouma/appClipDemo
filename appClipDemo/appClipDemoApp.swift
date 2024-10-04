dd8290450b5035d700dcae6e987d414b6ea2ca56
bengouma <bengoumalatsos@hotmail.com>
2024-10-04 10:44:38 -0400

    Initial Commit

---
+++ appClipDemo/appClipDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  appClipDemoApp.swift
+//  appClipDemo
+//
+//  Created by Ben Goumalatsos on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct appClipDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ appClipDemoTests/appClipDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  appClipDemoTests.swift
+//  appClipDemoTests
+//
+//  Created by Ben Goumalatsos on 10/4/24.
+//
+
+import Testing
+@testable import appClipDemo
+
+struct appClipDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
